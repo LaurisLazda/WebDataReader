ca3efb42fc15742606057570f5365cec98854db9
Janis Lazda <jlazda94@gmail.com>
2017-08-17 17:14:53 +0300

    Created objects from collected data

--- WebData/src/readhtml/RimiData.java
+++ WebData/src/readhtml/RimiData.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,11 +14,15 @@ public class RimiData {
 
 	// TODO: check if server returns answer, if not handle exception and delay
 	// search for ~5 minutes.
-	private List<String> products;
-	private List<Double> prices;
-	private List<String> urlList;
-	private List<String> urlSecondaryList;
-	private List<String> categoryIDs;
+
+	// private List<String> urlList;
+	private Map<String, String> urlList;
+	// private List<String> urlSecondaryList;
+	private Map<String, String> urlSecondaryList;
+	private List<WebProduct> products;
+	private WebProduct webProduct;
+	// private Map<String, String> categories;
+	private List<String> categories;
 
 	public static void main(String[] args) {
 		@SuppressWarnings("unused")
@@ -24,11 +30,13 @@ public class RimiData {
 	}
 
 	public RimiData() {
+		// urlList = new ArrayList<>();
+		urlList = new HashMap<>();
+		// urlSecondaryList = new ArrayList<>();
+		urlSecondaryList = new HashMap<>();
 		products = new ArrayList<>();
-		prices = new ArrayList<>();
-		urlList = new ArrayList<>();
-		categoryIDs = new ArrayList<>();
-		urlSecondaryList = new ArrayList<>();
+		// categories = new HashMap<>();
+		categories = new ArrayList<>();
 
 		try {
 			collectURLs();
@@ -45,8 +53,12 @@ public class RimiData {
 		Pattern pageCount = Pattern.compile("(?<=<b>1 / )(\\d{1,2})(?=</b>)");
 		Matcher matcher;
 		BufferedReader in;
+		String categoryName;
+		boolean isContent = false;
 
-		for (String string : urlList) {
+		// for (String string : urlList) {
+		for (String string : urlList.keySet()) {
+			categoryName = urlList.get(string);
 			boolean hasCategories = true;
 			url = new URL(string);
 			in = new BufferedReader(new InputStreamReader(url.openStream()));
@@ -67,33 +79,44 @@ public class RimiData {
 				String homeURL = "https://app.rimi.lv/products";
 				url = new URL(string);
 				Pattern subCategoryIdPattern = Pattern
-						.compile("(?<=data-category-id=\")(.+?)(?=\" class)");
+						.compile("(?<=<a href=\"/products/)(.+?)(?=\" data-category-id)");
 				in = new BufferedReader(new InputStreamReader(url.openStream()));
 
 				while ((inputLine = in.readLine()) != null) {
-					matcher = subCategoryIdPattern.matcher(inputLine);
-					if (matcher.find()) {
-						urlSecondaryList.add(homeURL + "/" + matcher.group());
+					if (isContent) {
+						if ((inputLine.replaceAll("\\s", "")).equals("</div>")) {
+							break;
+						}
+						matcher = subCategoryIdPattern.matcher(inputLine);
+						if (matcher.find()) {
+							urlSecondaryList.put(
+									homeURL + "/" + matcher.group(),
+									categoryName);
+						}
+					}
+					if ((inputLine.replaceAll("\\s", ""))
+							.equals("<divclass=\"collapsable-groupm-bottom01hidden-xs\">")) {
+						isContent = true;
 					}
 				}
 				in.close();
 			} else {
 				for (int i = 1; i <= pagesCount; i++) {
 					if (pagesCount == 1) {
-						readPage(url);
+						readPage(url, categoryName);
 					} else {
 						url = new URL(string + "/page/" + i);
-						readPage(url);
+						readPage(url, categoryName);
 					}
 				}
 			}
 		}
-		
-		for (String subUrl : urlSecondaryList) {
+		// for (String subUrl : urlSecondaryList) {
+		for (String subUrl : urlSecondaryList.keySet()) {
+			categoryName = urlSecondaryList.get(subUrl);
 			url = new URL(subUrl);
 			pagesCount = 0;
-			in = new BufferedReader(new InputStreamReader(
-					url.openStream()));
+			in = new BufferedReader(new InputStreamReader(url.openStream()));
 			while ((inputLine = in.readLine()) != null) {
 				if ((inputLine.replaceAll("\\s", ""))
 						.equals("<divclass=\"category-itemsjs-cat-items\">")) {
@@ -105,19 +128,22 @@ public class RimiData {
 				}
 			}
 			in.close();
-
 			for (int i = 1; i <= pagesCount; i++) {
 				if (pagesCount == 1) {
-					readPage(url);
+					readPage(url, categoryName);
 				} else {
 					url = new URL(subUrl + "/page/" + i);
-					readPage(url);
+					readPage(url, categoryName);
 				}
 			}
 		}
 	}
 
-	private void readPage(URL url) throws Exception {
+	private void readPage(URL url, String categoryName) throws Exception {
+		String product = "";
+		double price = 0;
+		long barcode = 0;
+
 		BufferedReader in = new BufferedReader(new InputStreamReader(
 				url.openStream()));
 		String inputLine;
@@ -126,12 +152,15 @@ public class RimiData {
 				.compile("(?<=whole-number \">)(\\d{1,3})(?=</span>)");
 		Pattern decimalPricePattern = Pattern
 				.compile("(?<=decimal\">)(\\d{2})(?=</span>)");
+		Pattern barcodePattern = Pattern
+				.compile("(?<=data-ean=\")(\\d+)(?=\" data-item_id=)");
 		Matcher matcher;
 
 		while ((inputLine = in.readLine()) != null) {
 			boolean isTable = false;
 			boolean isContent = false;
 			boolean priceBubble = false;
+			boolean barcodeFound = false;
 			String wholePrice;
 			String decimalPrice;
 			String allPrice;
@@ -142,14 +171,19 @@ public class RimiData {
 						if ((inputLine.replaceAll("\\s", "")).equals("</ul>")) {
 							break;
 						}
+						if (!barcodeFound) {
+							matcher = barcodePattern.matcher(inputLine);
+							if (matcher.find()) {
+								barcode = Long.parseLong(matcher.group());
+								barcodeFound = true;
+							}
+						}
 						if ((inputLine.replaceAll("\\s", ""))
 								.equals("<divclass=\"inforelativeclear\">")) {
 							inputLine = in.readLine();
 							matcher = namePattern.matcher(inputLine);
 							matcher.find();
-							products.add(matcher.group());
-							// temporary output for testing
-							// System.out.println(matcher.group());
+							product = matcher.group();
 						}
 						if ((inputLine.replaceAll("\\s", ""))
 								.equals("<divclass=\"price-bubble\">")) {
@@ -170,9 +204,11 @@ public class RimiData {
 							matcher.find();
 							decimalPrice = matcher.group();
 							allPrice = wholePrice + "." + decimalPrice;
-							prices.add(Double.parseDouble(allPrice));
-							// temporary output for testing
-							// System.out.println(allPrice);
+							price = Double.parseDouble(allPrice);
+							webProduct = new WebProduct(categoryName, product, price, barcode);
+							products.add(webProduct);
+							System.out.println(webProduct.toString()); //test
+							barcodeFound = false;
 						}
 					}
 					if (isTable) {
@@ -195,37 +231,67 @@ public class RimiData {
 	}
 
 	private void collectURLs() throws Exception {
+		boolean isContent = false;
 		String homeURL = "https://app.rimi.lv/products";
-		URL url = new URL(homeURL);
+		URL url = new URL(homeURL + "/selection");
 		String inputLine;
 		Pattern categoryIdPattern = Pattern
-				.compile("(?<=data-category-id=\")(.+?)(?=\" class)");
+				.compile("(?<=<a href=\"/products/)(.+?)(?=\" data-category-id)");
+		Pattern categoryNamePattern = Pattern
+				.compile("(?<=<span>)(.+?)(?=</span>)");
 		Matcher matcher;
 		BufferedReader in = new BufferedReader(new InputStreamReader(
 				url.openStream()));
-
+		
 		while ((inputLine = in.readLine()) != null) {
-			matcher = categoryIdPattern.matcher(inputLine);
-			if (matcher.find()) {
-				categoryIDs.add(matcher.group());
-				// temporary output for testing
-				// System.out.println(matcher.group());
+			if (isContent) {
+				if ((inputLine.replaceAll("\\s", "")).equals("</div>")) {
+					break;
+				}
+				matcher = categoryIdPattern.matcher(inputLine);
+				if (matcher.find()) {
+					categories.add(matcher.group());
+					// String categoryId = matcher.group();
+					// matcher = categoryNamePattern.matcher(inputLine);
+					// matcher.find();
+					// String categoryName = matcher.group();
+					// categories.put(categoryId, categoryName);
+				}
+			}
+			if ((inputLine.replaceAll("\\s", ""))
+					.equals("<divclass=\"collapsable-groupm-bottom01hidden-xs\">")) {
+				isContent = true;
 			}
 		}
 		in.close();
-
-		for (String string : categoryIDs) {
+		// for (String string : categories.keySet()) {
+		for (String string : categories) {
+			isContent = false;
 			url = new URL(homeURL + "/" + string);
-			Pattern subCategoryPattern = Pattern
-					.compile("(?<=data-category-id=\")(.+?)(?=\" class)");
+			// Pattern subCategoryPattern =
+			// Pattern.compile("(?<=data-category-id=\")(.+?)(?=\" class)");
 			in = new BufferedReader(new InputStreamReader(url.openStream()));
-
+			
 			while ((inputLine = in.readLine()) != null) {
-				matcher = subCategoryPattern.matcher(inputLine);
-				if (matcher.find()) {
-					urlList.add(homeURL + "/" + matcher.group());
-					// temporary output for testing
-					// System.out.println(matcher.group());
+				if (isContent) {
+					if ((inputLine.replaceAll("\\s", "")).equals("</div>")) {
+						break;
+					}
+					// matcher = subCategoryPattern.matcher(inputLine);
+					matcher = categoryIdPattern.matcher(inputLine);
+					if (matcher.find()) {
+						// urlList.add(homeURL + "/" + matcher.group());
+						String categoryId = matcher.group();
+						inputLine = in.readLine();
+						matcher = categoryNamePattern.matcher(inputLine);
+						matcher.find();
+						String categoryName = matcher.group();
+						urlList.put(homeURL + "/" + categoryId, categoryName);
+					}
+				}
+				if ((inputLine.replaceAll("\\s", ""))
+						.equals("<divclass=\"collapsable-groupm-bottom01hidden-xs\">")) {
+					isContent = true;
 				}
 			}
 			in.close();
